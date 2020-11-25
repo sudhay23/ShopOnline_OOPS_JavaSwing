99d44d4a4c14b9692245f9a37e296a5d0ed1df57
Sandeep Rajakrishnan <sandur43@gmail.com>
2020-11-25 22:56:31 +0530

    Profile panel needs to be added

---
+++ codes/CustomerArea.java
@@ -0,0 +1,350 @@
+import javax.swing.*;
+import javax.swing.table.*;
+
+import java.util.*;
+ import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+public class CustomerArea implements ActionListener {  
+JFrame f;  
+JTable jt = new JTable();
+DefaultTableModel model = (DefaultTableModel) jt.getModel();
+JScrollPane jsp= new JScrollPane(jt);
+
+JTabbedPane tp=new JTabbedPane();
+
+
+JLabel productIDCustomerLabel;
+JTextField productIDCustomerText;
+	
+JLabel productQuantityCustomerLabel;
+JTextField productQuantityCustomerText;
+
+JButton addToCartBtn;
+
+JLabel totalCostLabel;
+
+
+
+JTable jt1 = new JTable();
+DefaultTableModel model1 = (DefaultTableModel) jt1.getModel();
+JScrollPane jsp1= new JScrollPane(jt1);
+
+
+
+
+JPanel p1;
+JPanel  p2;
+JPanel p3;
+
+
+int totalCost=0;
+
+JButton clearCartBtn;
+JButton placeOrderBtn;
+
+public void productTable(){
+
+
+	jsp.setBounds(30,50,700,400);
+
+jt.setBounds(30,50,700,400);
+
+
+
+ model.addColumn("Product ID");
+        model.addColumn("Product Name");
+            model.addColumn("Product Price");
+    model.addColumn("Stock");
+    model.addColumn("Product Description");
+
+
+    
+
+
+	p1.add(jsp);
+
+	getFromFile();
+
+
+
+		
+
+
+
+
+	}
+
+public  void getFromFile(){
+		 File directoryPath = new File("C:\\Users\\USER\\Desktop\\java\\oop sem peoject\\gui\\productInfo");
+      //List of all files and directories
+      String contents[] = directoryPath.list();
+      for(int i=0; i<contents.length; i++) {
+        
+        String currentFile=contents[i];
+        System.out.println(currentFile);
+
+        try{
+        File myObj = new File("C:\\Users\\USER\\Desktop\\java\\oop sem peoject\\gui\\productInfo\\"+currentFile);
+
+        Scanner myReader = new Scanner(myObj);
+         while (myReader.hasNextLine()) {
+        String productIDdata = myReader.nextLine().split(":")[1];
+        String productNameData =myReader.nextLine().split(":")[1];
+        String productPriceData =myReader.nextLine().split(":")[1]; 
+        String productStockData =myReader.nextLine().split(":")[1]; 
+        String productDescriptionData =myReader.nextLine().split(":")[1]; 
+
+        System.out.println(productIDdata+" "+productNameData+" "+ productPriceData+" "+ productStockData+" "+ productDescriptionData); 
+        //System.out.println(data);
+
+        model.insertRow(model.getRowCount(), new Object[] { productIDdata, productNameData, productPriceData, productStockData, productDescriptionData });
+
+
+
+
+      }
+
+
+
+        myReader.close();
+        }
+         catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+   }
+
+	}
+
+public CustomerArea() { 
+    f=new JFrame("Welcome to shopOnline-Customer");  
+    f.setSize(800,600);
+    
+     p1=new JPanel();  
+      
+     p2=new JPanel();  
+     p3=new JPanel();  
+       
+    tp.setBounds(0,0,600,600);  
+
+    tp.add("Browse Products",p1);  
+    tp.add("Your Cart",p2);  
+    tp.add("Your Profile",p3); 
+    p1.setLayout(null); 
+    p2.setLayout(null);
+    p3.setLayout(null);
+
+
+
+
+
+
+
+
+
+
+
+    f.add(tp);  
+//    f.setLayout(null);  
+    f.setVisible(true);  
+
+    productIDCustomerLabel= new JLabel("Product ID : ");
+	productIDCustomerLabel.setBounds(40,480,80,25);
+	p1.add(productIDCustomerLabel);
+
+	productIDCustomerText = new JTextField(20);
+	productIDCustomerText.setBounds(120, 480, 160 , 25);
+	p1.add(productIDCustomerText);
+
+	productQuantityCustomerLabel= new JLabel("Quantity : ");
+	productQuantityCustomerLabel.setBounds(300,480,80,25);
+	p1.add(productQuantityCustomerLabel);
+
+	productQuantityCustomerText = new JTextField(20);
+	productQuantityCustomerText.setBounds(375, 480, 160 , 25);
+	p1.add(productQuantityCustomerText);
+
+	addToCartBtn= new JButton("Add to Cart");
+	addToCartBtn.setBounds(565,480,160,25);
+	addToCartBtn.addActionListener(this);
+	p1.add(addToCartBtn);   
+
+	totalCostLabel=new JLabel("");
+	totalCostLabel.setBounds(630,480,160,25);
+	p2.add(totalCostLabel); 
+
+
+	clearCartBtn=new JButton("Clear Cart");
+	clearCartBtn.setBounds(30,480,150,25);
+	clearCartBtn.addActionListener(this);
+	p2.add(clearCartBtn);
+
+	placeOrderBtn=new JButton("Place Order");
+	placeOrderBtn.setBounds(320,480,150,25);
+	placeOrderBtn.addActionListener(this);
+	p2.add(placeOrderBtn);
+
+
+
+
+    
+}  
+
+public void cartTabFileHandling(String productID , String productQuantity){
+
+	try{
+        File myObj = new File("C:\\Users\\USER\\Desktop\\java\\oop sem peoject\\gui\\productInfo\\"+productID+".txt");
+
+        Scanner myReader = new Scanner(myObj);
+         while (myReader.hasNextLine()) {
+        String productIDdata = myReader.nextLine().split(":")[1];
+        String productNameData =myReader.nextLine().split(":")[1];
+        String productPriceData =myReader.nextLine().split(":")[1]; 
+
+        String productStockData =myReader.nextLine().split(":")[1]; 
+        String productDescriptionData =myReader.nextLine().split(":")[1]; 
+
+             
+
+        int cost=Integer.parseInt(productPriceData.trim())*Integer.parseInt(productQuantity.trim());
+        totalCost+=cost;
+
+        totalCostLabel.setText("Total Cost = "+totalCost);
+
+        
+
+       // System.out.println(cost);
+
+        //System.out.println(productIDdata+" "+productNameData+" "+ productPriceData+" "+ productStockData+" "+ productDescriptionData); 
+        //System.out.println(data);
+
+
+        model1.insertRow(model1.getRowCount(), new Object[] { productIDdata, productNameData, productPriceData, productQuantity, Integer.toString(cost) });
+
+
+
+
+      }
+
+
+
+        myReader.close();
+        }
+         catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+   }
+
+
+
+
+
+
+public void createCartTable(){
+
+		jsp1.setBounds(30,50,700,400);
+
+jt1.setBounds(30,50,700,400);
+
+
+
+ model1.addColumn("Product ID");
+        model1.addColumn("Product Name");
+            model1.addColumn("Product Price");
+    model1.addColumn("Quantity");
+    model1.addColumn("Cost");
+
+
+    
+
+
+	p2.add(jsp1);
+
+
+}
+
+
+
+
+
+public static void main(String[] args) {  
+	
+CustomerArea tabObj =   new CustomerArea(); 
+tabObj.productTable();
+tabObj.createCartTable();
+
+
+}
+
+@Override
+public void actionPerformed(ActionEvent e){
+
+	if(e.getSource()==addToCartBtn){
+
+	
+
+	/*System.out.println(productIDCustomerText.getText());
+	System.out.println(productQuantityCustomerText.getText());*/
+
+String productIdCustomer=productIDCustomerText.getText();
+String productQuantity=productQuantityCustomerText.getText();
+
+System.out.println(productIdCustomer);
+System.out.println(productQuantity);
+
+
+if(productIdCustomer.trim().length()==0 || productQuantity.trim().length()==0){
+JOptionPane.showMessageDialog(p1, "Fill all fields",
+"Warning", JOptionPane.WARNING_MESSAGE);
+
+
+}
+
+else{
+	cartTabFileHandling(productIdCustomer.trim(),productQuantity.trim());
+	model.setRowCount(0);
+	getFromFile();
+
+}
+
+ 
+
+
+
+
+}
+
+else if(e.getSource()==clearCartBtn){
+
+	 int a=JOptionPane.showConfirmDialog(p2,"Are you sure?");  
+if(a==JOptionPane.YES_OPTION){  
+    model1.setRowCount(0);
+	totalCostLabel.setText(""); 
+	totalCost=0; 
+}  
+
+
+
+	
+}
+
+else if(e.getSource()==placeOrderBtn){
+	if(model1.getRowCount()==0){
+		JOptionPane.showMessageDialog(p2, "Your cart is empty, add products to your cart",
+"Warning", JOptionPane.WARNING_MESSAGE);
+
+	}
+	else{
+		JOptionPane.showMessageDialog(p2, "Thank You, Order placed successfully",
+"Warning", JOptionPane.WARNING_MESSAGE);
+		model1.setRowCount(0);
+		totalCostLabel.setText("");
+		totalCost=0;
+
+	}
+}
+
+
+}  }
---
+++ codes/productsGUI.java
@@ -0,0 +1,317 @@
+import javax.swing.*;
+import javax.swing.table.*;
+
+import java.util.*;
+ import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+public class productsGUI implements ActionListener{
+	
+	JFrame frame;
+	JPanel panel;
+	JButton button;
+	JLabel headingLabel;
+	JTable jt = new JTable();
+	  
+
+
+	JLabel productIDLabel;
+	JTextField productIDText;
+	
+	JLabel productNameLabel;
+	JTextField productNameText;
+
+	JLabel productPriceLabel;
+	JTextField productPriceText;
+	
+
+	JLabel stockLabel;
+	JTextField stockText;
+
+
+
+	JLabel productDescriptionLabel;
+	JTextArea productDescriptionText;
+
+	    DefaultTableModel model = (DefaultTableModel) jt.getModel();
+   
+
+
+
+    JScrollPane jsp= new JScrollPane(jt);
+    JButton btn;
+
+
+
+   static  int exist=0;
+
+
+
+	//Jlabel registrationStatus;
+
+	int i=0;
+	public String data[][]=new String[100][5];
+
+	public  void getFromFile(){
+		 File directoryPath = new File("C:\\Users\\USER\\Desktop\\java\\oop sem peoject\\gui\\productInfo");
+      //List of all files and directories
+      String contents[] = directoryPath.list();
+      for(int i=0; i<contents.length; i++) {
+        
+        String currentFile=contents[i];
+        System.out.println(currentFile);
+
+        try{
+        File myObj = new File("C:\\Users\\USER\\Desktop\\java\\oop sem peoject\\gui\\productInfo\\"+currentFile);
+
+        Scanner myReader = new Scanner(myObj);
+         while (myReader.hasNextLine()) {
+        String productIDdata = myReader.nextLine().split(":")[1];
+        String productNameData =myReader.nextLine().split(":")[1];
+        String productPriceData =myReader.nextLine().split(":")[1]; 
+        String productStockData =myReader.nextLine().split(":")[1]; 
+        String productDescriptionData =myReader.nextLine().split(":")[1]; 
+
+        System.out.println(productIDdata+" "+productNameData+" "+ productPriceData+" "+ productStockData+" "+ productDescriptionData); 
+        //System.out.println(data);
+
+        model.insertRow(model.getRowCount(), new Object[] { productIDdata, productNameData, productPriceData, productStockData, productDescriptionData });
+
+
+
+
+      }
+
+
+
+        myReader.close();
+        }
+         catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+   }
+
+	}
+
+	public productsGUI() {
+	
+	frame = new JFrame("shopOnline - ADMIN");
+	frame.setSize(800,600);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setVisible(true);
+
+	panel = new JPanel();
+
+	frame.add(panel);
+
+	panel.setLayout(null);
+
+	headingLabel= new JLabel("Inventory management");
+	headingLabel.setBounds(340,5,180,50);
+	panel.add(headingLabel);
+
+
+	productIDLabel = new JLabel("Product ID : ");
+	productIDLabel.setBounds(40,45,80,25);
+	panel.add(productIDLabel);
+
+	productIDText = new JTextField(20);
+	productIDText.setBounds(150, 45, 160 , 25);
+	panel.add(productIDText);
+
+
+
+	productNameLabel = new JLabel("Product name : ");
+	productNameLabel.setBounds(40,75,100,25);
+	panel.add(productNameLabel);
+
+	productNameText = new JTextField(20);
+	productNameText.setBounds(150, 75, 160 , 25);
+	panel.add(productNameText);
+
+
+	productPriceLabel = new JLabel("Price : ");
+	productPriceLabel.setBounds(40,105,80,25);
+	panel.add(productPriceLabel);
+
+	productPriceText = new JTextField(20);
+	productPriceText.setBounds(150, 105, 160 , 25);
+	panel.add(productPriceText);
+
+	stockLabel = new JLabel("Stock : ");
+	stockLabel.setBounds(40,135,80,25);
+	panel.add(stockLabel);
+
+	stockText = new JTextField(20);
+	stockText.setBounds(150, 135, 160 , 25);
+	panel.add(stockText);
+
+	productDescriptionLabel = new JLabel("Description : ");
+	productDescriptionLabel.setBounds(40, 165, 100 , 25);
+	panel.add(productDescriptionLabel);
+
+	productDescriptionText = new JTextArea();
+	productDescriptionText.setBounds(150, 165, 160 , 25);
+	panel.add(productDescriptionText);
+
+
+
+
+	
+
+	button= new JButton("Add to Inventory");
+	button.setBounds(130,205,160,25);
+	button.addActionListener(this);
+	panel.add(button);
+
+
+	
+	
+
+
+	}
+
+	public void productTable(){
+
+		 jt.setBounds(0,40,650,300);
+
+ model.addColumn("Product ID");
+        model.addColumn("Product Name");
+            model.addColumn("Product Price");
+    model.addColumn("Stock");
+    model.addColumn("Product Description");
+
+
+    
+
+jsp.setBounds(40,340,650,300);
+	panel.add(jsp);
+
+	getFromFile();
+
+		
+
+
+
+
+	}
+
+
+
+public static void createFile(String productID, String productName, String productPrice, String stock, String productDescription){
+
+		 try {
+      File checkFile = new File("C:\\Users\\USER\\Desktop\\java\\oop sem peoject\\gui\\productInfo\\"+productID+".txt");
+
+      if(checkFile.exists()){
+      	exist=1;
+
+      }
+      else{
+      	      FileWriter myWriter = new FileWriter("C:\\Users\\USER\\Desktop\\java\\oop sem peoject\\gui\\productInfo\\"+productID+".txt");
+      	      myWriter.write("ProductID:"+productID+"\n");
+      	      
+      	      myWriter.write("ProductName:"+productName+"\n");
+      	      myWriter.write("productPrice:"+productPrice+"\n");
+
+      	      myWriter.write("ProductStock:"+stock+"\n");
+      	      myWriter.write("productDescription:"+productDescription+"\n");
+
+
+
+      			myWriter.close();
+
+      		
+
+      }
+
+
+   /*   if (myObj.createNewFile()) {
+        System.out.println("File created: " + myObj.getName());
+      } else {
+        System.out.println("File already exists.");
+      }*/
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+
+
+
+	}
+
+
+
+	public static void main(String args[]){
+	productsGUI obj=new productsGUI();  
+	obj.productTable();
+	
+
+
+	}
+	@Override
+	public void actionPerformed(ActionEvent e){
+		/*++i;
+		data[i][0]=productIDText.getText();
+		data[i][1]=productNameText.getText();
+		data[i][2]=productPriceText.getText();
+		data[i][3]=stockText.getText();
+		data[i][4]=productDescriptionText.getText();
+		productTable();*/
+
+
+	String productID=productIDText.getText();
+	String productName=productNameText.getText();
+		String productPrice=productPriceText.getText();
+		String stock=stockText.getText();
+		String productDescription=productDescriptionText.getText();
+
+		if(productID.trim().length()==0 || productName.trim().length()==0 || productPrice.trim().length()==0 || stock.trim().length()==0 ||productDescription.trim().length()==0 ) 
+		{
+			JOptionPane.showMessageDialog(frame, "Fill all fields",
+               "Warning", JOptionPane.WARNING_MESSAGE);
+
+			
+
+
+		}
+		else{
+					createFile(productID, productName, productPrice, stock, productDescription);
+
+		
+
+
+
+
+		
+
+if (exist==0 && productID.trim().length()!=0) {
+	model.insertRow(model.getRowCount(), new Object[] { productID, productName, productPrice, stock, productDescription });
+
+	
+}
+else{
+	exist=0;
+
+	 JOptionPane.showMessageDialog(frame, "Product already exists!",
+               "Warning", JOptionPane.WARNING_MESSAGE);
+
+}
+
+
+    // clear the entries.
+}
+
+}
+}
+
+
+
+
+
+
+
+
+
+
